d7639db5172bd7a76f2a62307cf918371f8d46f5
cao-awa <cao.awa.www@gmail.com>
2025-03-27 17:47:44 +0800

    Discard the leash knot randomly.

--- common/src/main/java/com/github/cao/awa/lycoris/Lycoris.java
+++ common/src/main/java/com/github/cao/awa/lycoris/Lycoris.java
@@ -3,9 +3,12 @@ package com.github.cao.awa.lycoris;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.Random;
+
 public class Lycoris {
+    public static final Random RANDOM = new Random();
     public static final Logger LOGGER = LogManager.getLogger("Lycoris");
-    public static final String VERSION = "1.0.13";
+    public static final String VERSION = "1.1.0";
 
     public static void init() {
 
---
+++ common/src/main/java/com/github/cao/awa/lycoris/mixin/entity/block/BlockAttachedEntityMixin.java
@@ -0,0 +1,27 @@
+package com.github.cao.awa.lycoris.mixin.entity.block;
+
+import com.github.cao.awa.lycoris.Lycoris;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.decoration.BlockAttachedEntity;
+import net.minecraft.entity.decoration.LeashKnotEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(BlockAttachedEntity.class)
+public class BlockAttachedEntityMixin {
+    @Inject(
+            method = "tick",
+            at = @At("RETURN")
+    )
+    public void randomDiscard(CallbackInfo ci) {
+        // Let leash knot randomly discard.
+        if (((Entity) (Object) this) instanceof LeashKnotEntity leashKnot) {
+            // Only 1000 of 1.
+            if (Lycoris.RANDOM.nextInt(1000) == 0) {
+                leashKnot.discard();
+            }
+        }
+    }
+}
