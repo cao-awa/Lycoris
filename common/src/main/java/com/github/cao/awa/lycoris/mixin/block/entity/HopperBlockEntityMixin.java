4918315208c6b4e58748457b175b0a2f1bfda0fa
hotpad100c <46704388+hotpad100c@users.noreply.github.com>
2025-04-08 21:36:38 +0900

    FUnny Hopper & Funny totem

---
+++ common/src/main/java/com/github/cao/awa/lycoris/mixin/block/entity/HopperBlockEntityMixin.java
@@ -0,0 +1,49 @@
+package com.github.cao.awa.lycoris.mixin.block.entity;
+
+import com.github.cao.awa.lycoris.Lycoris;
+import com.github.cao.awa.lycoris.tree.TreeAnalyzer;
+import com.llamalad7.mixinextras.sugar.Local;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.SideShapeType;
+import net.minecraft.block.entity.HopperBlockEntity;
+import net.minecraft.entity.FallingBlockEntity;
+import net.minecraft.entity.ItemEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Debug;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+@Mixin(HopperBlockEntity.class)
+public class HopperBlockEntityMixin {
+    @Inject(
+            method = "insert",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/item/ItemStack;getCount()I"
+            ),cancellable = true
+    )
+    private static void insert(World world, BlockPos pos, HopperBlockEntity blockEntity,
+                               CallbackInfoReturnable<Boolean> cir, @Local Direction direction,
+                               @Local ItemStack itemstack,@Local int i) {
+        //Drops item if back of the hopper isn't blocked
+        BlockState blockState = world.getBlockState(blockEntity.getPos().offset(direction));
+        if(blockState.isAir()) {
+            ItemStack stack = itemstack.copy();
+            stack.setCount(Lycoris.RANDOM.nextInt(1,Math.max(2,(int)itemstack.getCount()/2)));
+            blockEntity.removeStack(i, stack.getCount());
+            Block.dropStack(world, blockEntity.getPos().offset(direction), stack);
+            cir.setReturnValue(true);
+        }
+    }
+}
---
+++ common/src/main/java/com/github/cao/awa/lycoris/mixin/entity/LivingEntityMixin.java
@@ -0,0 +1,83 @@
+package com.github.cao.awa.lycoris.mixin.entity;
+
+import com.github.cao.awa.lycoris.Lycoris;
+import com.github.cao.awa.lycoris.config.LycorisConfig;
+import net.minecraft.advancement.criterion.Criteria;
+import net.minecraft.component.DataComponentTypes;
+import net.minecraft.component.type.DeathProtectionComponent;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.data.DataTracker;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.consume.ConsumeEffect;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.registry.tag.DamageTypeTags;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.stat.Stats;
+import net.minecraft.util.Hand;
+import net.minecraft.world.World;
+import net.minecraft.world.event.GameEvent;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(LivingEntity.class)
+public abstract class LivingEntityMixin {
+
+    @Shadow public abstract void setHealth(float health);
+
+    @Inject(
+            method = "tryUseDeathProtector",
+            at = @At(
+                    value = "HEAD"
+            ),
+            cancellable = true
+    )
+    public void tryUseDeathProtector(DamageSource source, CallbackInfoReturnable<Boolean> cir) {
+        //Stop entity from dying is attacker has a totem in hand
+        if (!source.isIn(DamageTypeTags.BYPASSES_INVULNERABILITY)){
+            Entity attacker = source.getAttacker();
+            boolean cantDie = false;
+            ItemStack itemStack = null;
+            DeathProtectionComponent deathProtectionComponent = null;
+
+            if(attacker instanceof LivingEntity livingAttacker) {
+                if (attacker instanceof ServerPlayerEntity serverPlayerEntity) {
+                    itemStack = serverPlayerEntity.getStackInHand(Hand.OFF_HAND);
+                    deathProtectionComponent = itemStack.get(DataComponentTypes.DEATH_PROTECTION);
+                    if(deathProtectionComponent != null) {
+                        serverPlayerEntity.incrementStat(Stats.USED.getOrCreateStat(itemStack.getItem()));
+                        Criteria.USED_TOTEM.trigger(serverPlayerEntity, itemStack);
+                        serverPlayerEntity.emitGameEvent(GameEvent.ITEM_INTERACT_FINISH);
+                        cantDie = true;
+                    }
+                }else{
+                    for (Hand hand : Hand.values()) {
+                        itemStack = livingAttacker.getStackInHand(hand);
+                        deathProtectionComponent = itemStack.get(DataComponentTypes.DEATH_PROTECTION);
+                        if (deathProtectionComponent != null) {
+                            itemStack.decrement(1);
+                            cantDie = true;
+                            break;
+                        }
+                    }
+                }
+            }
+            if(cantDie) {
+                this.setHealth(1.0F);
+                itemStack.decrement(1);
+                deathProtectionComponent.applyDeathEffects(itemStack, (LivingEntity)(Object)this);
+                attacker.getWorld().sendEntityStatus((LivingEntity)(Object)this, (byte)35);
+                cir.setReturnValue(true);
+            }
+        }
+    }
+}
